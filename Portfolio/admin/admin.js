a486c75fe7fc4d71439c75df3c43b865472d1d46
Zzu-h <72387349+Zzu-h@users.noreply.github.com>
2020-10-06 02:03:54 +0900

    Add files via upload

---
+++ Portfolio/admin/admin.js
@@ -0,0 +1,15 @@
+function go(num){
+    if(num == 3){
+        location.href="/cono/src/project/Intro.html";
+    }
+    else if (num == 2){
+        location.href="List.html";
+    }
+    else if(num == 1){
+        location.href="content_editor.html";
+    }
+    else if(num == 0){
+        location.href="menu_editor.html";
+    }
+
+}
